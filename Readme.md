1b809d01b3bb00e94f63631a83ee3063dc3761b7
zhuqiacheng <zhuqiacheng@meizu.com>
2016-07-01 15:01:45 +0800

    fist

---
+++ Readme.md
@@ -0,0 +1,86 @@
+<!-- [![NPM version][npm-img]][npm-url]
+[![Build status][travis-img]][travis-url]
+[![Test coverage][coveralls-img]][coveralls-url]
+[![License][license-img]][license-url]
+[![Dependency status][david-img]][david-url] -->
+
+### gulp-html-url-prefix
+a plugin of gulp for prefix url
+
+### install
+```bash
+npm install gulp-html-url-prefix
+```
+
+### options
+
+* options - type: `object`
+  - prefix: `string`, default ``
+
+```js
+urlPrefix({
+  prefix: '//cdn.xxx.com'
+})
+```
+
+### example
+
+index.html
+```html
+<!DOCTYPE html>
+<html>
+  <head>
+    <link href="./css/example.css" />
+  </head>
+  <body>
+    <img src="./example.jpg" />
+    <script type="text/javascript" src="./js/example.js"></script>
+  </body>
+</html>
+```
+
+gulpfile.js
+```js
+var urlPrefix = require('gulp-html-url-prefix'),
+  gulp = require('gulp');
+
+gulp.task('url', function() {
+  gulp.src(['index.html'])
+    .pipe(urlPrefix({
+      prefix: '//cdn.xxx.com'
+    }))
+    .pipe(gulp.dest('./'));
+});
+```
+
+and the result is:
+```html
+<!DOCTYPE html>
+<html>
+  <head>
+    <link href="//cdn.xxx.com/css/example.css" />
+  </head>
+  <body>
+    <img src="//cdn.xxx.com/example.jpg" />
+    <script type="text/javascript" src="//cdn.xxx.com/js/example.js"></script>
+  </body>
+</html>
+```
+
+
+### License
+MIT
+<!-- 
+[npm-img]: https://img.shields.io/npm/v/gulp-file-include.svg?style=flat-square
+[npm-url]: https://npmjs.org/package/gulp-file-include
+[travis-img]: https://img.shields.io/travis/coderhaoxin/gulp-file-include.svg?style=flat-square
+[travis-url]: https://travis-ci.org/coderhaoxin/gulp-file-include
+[coveralls-img]: https://img.shields.io/coveralls/coderhaoxin/gulp-file-include.svg?style=flat-square
+[coveralls-url]: https://coveralls.io/r/coderhaoxin/gulp-file-include?branch=master
+[license-img]: http://img.shields.io/badge/license-MIT-green.svg?style=flat-square
+[license-url]: http://opensource.org/licenses/MIT
+[david-img]: https://img.shields.io/david/coderhaoxin/gulp-file-include.svg?style=flat-square
+[david-url]: https://david-dm.org/coderhaoxin/gulp-file-include
+[gitter-img]: https://badges.gitter.im/Join%20Chat.svg
+[gitter-url]: https://gitter.im/coderhaoxin/gulp-file-include?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge
+ -->
---
+++ index.js
@@ -0,0 +1,88 @@
+'use strict';
+
+var attrParse = require("./lib/attributesParser");
+var concat = require('concat-stream');
+var through = require('through2');
+var gutil = require('gulp-util');
+var fs = require('fs');
+var url = require('url');
+
+
+module.exports = function(opts) {
+
+    var prefix = opts && opts.prefix || "";
+    var attrs = ["img:src", "img:srcset", "img:data-src", "script:src", "link:href"];
+
+    function fileInclude(file, enc, cb) {
+        if (file.isNull()) {
+            cb(null, file);
+        } else if (file.isStream()) {
+            file.contents.pipe(concat(function(data) {
+                try {
+                    data = include(file, String(data));
+                    cb(null, data);
+                } catch (e) {
+                    cb(new gutil.PluginError('gulp-html-url-prefix', e.message));
+                }
+            }));
+        } else if (file.isBuffer()) {
+            try {
+                file = include(file, String(file.contents));
+                cb(null, file);
+            } catch (e) {
+                cb(new gutil.PluginError('gulp-html-url-prefix', e.message));
+            }
+        }
+    }
+
+    function include(file, content, data) {
+
+        var rawLinks = attrParse(content, function(tag, attr) {
+            return attrs.indexOf(tag + ":" + attr) >= 0;
+        });
+        var links = [];
+        rawLinks.forEach(function(link) {
+            var length = link.length;
+            var start = link.start;
+            var valueList = link.value.split(",");
+            valueList.forEach(function(newLink) {
+                var trimmed = newLink.trim();
+                var cLength = newLink.length;
+                var spacePos = trimmed.indexOf(" ");
+                var spaceStart = newLink.indexOf(trimmed);
+                var len = cLength + spaceStart;
+                if (-1 != spacePos) {
+                    len = spacePos + spaceStart;
+                    trimmed = trimmed.substring(0, spacePos);
+                }
+                links.push({
+                    start: start,
+                    length: len,
+                    value: trimmed
+                });
+                start += cLength + 1;
+            });
+        });
+
+        links.reverse();
+        content = [content];
+        links.forEach(function(link) {
+            if (/^http[s]?:\/\/|^\/\//i.test(link.value)) return;
+            var value = url.resolve(prefix, link.value);
+
+            var x = content.pop();
+            content.push(x.substr(link.start + link.length));
+            content.push(value);
+            content.push(x.substr(0, link.start));
+        });
+        content.reverse();
+        content = content.join("");
+
+        file.contents = new Buffer(content);
+
+        return file;
+    }
+
+    return through.obj(fileInclude);
+
+}
